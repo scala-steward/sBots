4f8dfd9d7ba3073a101780ce9f7eba6783aa9d81
Enrico Benini <benkio89@gmail.com>
2026-02-23 19:37:23 +0000

    🚚 `File` to `Path` into GenerateTriggers`

--- modules/main/src/test/scala/com/benkio/main/GenerateTriggersSpec.scala
+++ modules/main/src/test/scala/com/benkio/main/GenerateTriggersSpec.scala
@@ -2,14 +2,10 @@ package com.benkio.main
 
 import cats.effect.ExitCode
 import com.benkio.telegrambotinfrastructure.SBot
-import com.benkio.ABarberoBot.ABarberoBot
-import com.benkio.CalandroBot.CalandroBot
-import com.benkio.M0sconiBot.M0sconiBot
-import com.benkio.RichardPHJBensonBot.RichardPHJBensonBot
-import com.benkio.YouTuboAncheI0Bot.YouTuboAncheI0Bot
 import munit.CatsEffectSuite
 
-import java.io.*
+import java.nio.file.Files
+import java.nio.file.Path
 import scala.concurrent.duration.Duration
 
 class GenerateTriggersSpec extends CatsEffectSuite {
@@ -17,18 +13,10 @@ class GenerateTriggersSpec extends CatsEffectSuite {
   override val munitIOTimeout = Duration(1, "m")
 
   test("GenerateTriggers.run should modify the expected files") {
-    val calaSBotConfig           = SBot.buildSBotConfig(CalandroBot.sBotInfo)
-    val abarSBotConfig           = SBot.buildSBotConfig(ABarberoBot.sBotInfo)
-    val mosSBotConfig            = SBot.buildSBotConfig(M0sconiBot.sBotInfo)
-    val ytaiSBotConfig           = SBot.buildSBotConfig(YouTuboAncheI0Bot.sBotInfo)
-    val rphjbSBotConfig          = SBot.buildSBotConfig(RichardPHJBensonBot.sBotInfo)
-    val triggerFiles: List[File] = List(
-      File(s"../bots/ABarberoBot/${abarSBotConfig.triggerFilename}"),
-      File(s"../bots/M0sconiBot/${mosSBotConfig.triggerFilename}"),
-      File(s"../bots/CalandroBot/${calaSBotConfig.triggerFilename}"),
-      File(s"../bots/RichardPHJBensonBot/${rphjbSBotConfig.triggerFilename}"),
-      File(s"../bots/YouTuboAncheI0Bot/${ytaiSBotConfig.triggerFilename}")
-    )
+    val triggerFiles: List[Path] = BotRegistry.toList(BotRegistry.value).map { entry =>
+      val config = SBot.buildSBotConfig(entry.sBotInfo)
+      Path.of(s"../bots/${entry.sBotInfo.botName.value}/${config.triggerFilename}")
+    }
     val fiveMinutesAgo = System.currentTimeMillis() - (5 * 60 * 1000)
     GenerateTriggers
       .run(List.empty)
@@ -36,10 +24,10 @@ class GenerateTriggersSpec extends CatsEffectSuite {
 
         assert(exitCode == ExitCode.Success)
         triggerFiles.foreach { file =>
-          assert(file.exists(), s"${file.getAbsolutePath} should exist")
+          assert(Files.exists(file), s"${file.toAbsolutePath().toString()} should exist")
           assert(
-            file.lastModified() >= fiveMinutesAgo,
-            s"${file.getAbsolutePath} should have been modified in the last 5 minutes (lastModified=${file.lastModified()}, fiveMinutesAgo=$fiveMinutesAgo)"
+            Files.getLastModifiedTime(file).toMillis() >= fiveMinutesAgo,
+            s"${file.toAbsolutePath().toString()} should have been modified in the last 5 minutes (lastModified=${Files.getLastModifiedTime(file).toMillis()}, fiveMinutesAgo=$fiveMinutesAgo)"
           )
         }
       })
