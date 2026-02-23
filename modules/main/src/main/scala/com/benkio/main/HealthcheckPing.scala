8bab23d96013cd6aa4401759ca2e71d3c48d21d2
Enrico Benini <benkio89@gmail.com>
2026-02-23 18:12:16 +0000

    ✅ validate

--- modules/main/src/main/scala/com/benkio/main/HealthcheckPing.scala
+++ modules/main/src/main/scala/com/benkio/main/HealthcheckPing.scala
@@ -1,16 +1,25 @@
 package com.benkio.main
 
+import cats.effect.implicits.*
+import cats.effect.Async
+import cats.implicits.*
 import cron4s.CronExpr
+import eu.timepit.fs2cron.cron4s.Cron4sScheduler
+import fs2.Stream
 import org.http4s.client.Client
+import org.http4s.Request
 import org.http4s.Uri
 
 object HealthcheckPing {
 
-  // TODO: Use `cron4s` to schedule a ping every X time.
-  // Get the config from outside for the cron.
-  // Spawn a non-cancellable fiber
-  // Add it to the webhook main
-  def healthcheckPing[F[_]](client: Client[F], healthcheckEndpoint: Uri, healthcheckCron: CronExpr): F[Unit] = {
-    ???
+  def healthcheckPing[F[_]: Async](client: Client[F], healthcheckEndpoint: Uri, healthcheckCron: CronExpr): F[Unit] = {
+    val cronScheduler     = Cron4sScheduler.systemDefault[F]
+    val healthcheckStream =
+      cronScheduler.awakeEvery(healthcheckCron) >>
+        Stream.eval(sendHealthcheckEndpoint(client, healthcheckEndpoint).void)
+    healthcheckStream.compile.drain.start.void
   }
+
+  def sendHealthcheckEndpoint[F[_]](client: Client[F], healthcheckEndpoint: Uri): F[Boolean] =
+    client.successful(Request(uri = healthcheckEndpoint))
 }
