fb75889eeb7e6e41221b7c2874f6a22c375e349a
Enrico Benini <benkio89@gmail.com>
2026-02-28 02:35:12 +0000

    DataReply writes directly into the bot replies file

--- modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/SBotDataEntry.scala
+++ modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/SBotDataEntry.scala
@@ -4,18 +4,10 @@ import cats.effect.*
 import com.benkio.telegrambotinfrastructure.config.SBotConfig
 import com.benkio.telegrambotinfrastructure.dataentry.DataEntry
 
-import java.nio.file.Paths
-
 object SBotDataEntry {
 
-  def run(args: List[String], sBotConfig: SBotConfig): IO[String] = {
-    val abarListFileResource =
-      Resource.make(IO.delay(scala.io.Source.fromFile(sBotConfig.listJsonFilename)))(bufferedSorce =>
-        IO.delay(bufferedSorce.close)
-      )
-
+  def run(args: List[String], sBotConfig: SBotConfig): IO[Unit] =
     DataEntry
-      .dataEntryLogic(args, abarListFileResource, Paths.get(sBotConfig.listJsonFilename))
-  }
+      .dataEntryLogic(args, sBotConfig)
 
 }
--- modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/dataentry/DataEntry.scala
+++ modules/telegramBotInfrastructure/src/main/scala/com/benkio/telegrambotinfrastructure/dataentry/DataEntry.scala
@@ -10,15 +10,25 @@ import io.circe.Json
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import java.nio.file.Path
-import scala.io.BufferedSource
+import com.benkio.telegrambotinfrastructure.config.SBotConfig
+import java.nio.file.Paths
 
 object DataEntry {
 
   private[dataentry] def parseInput(links: List[String]): IO[List[MediaFileSource]] =
     links.traverse(link => MediaFileSource.fromUriString(link.replace("dl=0", "dl=1")))
 
-  def dataEntryLogic(input: List[String], jsonFileResource: Resource[IO, BufferedSource], jsonFilePath: Path) =
+  def dataEntryLogic(input: List[String], sBotConfig: SBotConfig) = {
+    val jsonListFileResource =
+      Resource.make(IO.delay(scala.io.Source.fromFile(sBotConfig.listJsonFilename)))(bufferedSorce =>
+        IO.delay(bufferedSorce.close)
+      )
+    val listJsonFilepath = Paths.get(sBotConfig.listJsonFilename)
+    val repliesJsonFileResource =
+      Resource.make(IO.delay(scala.io.Source.fromFile(sBotConfig.repliesJsonFilename)))(bufferedSorce =>
+        IO.delay(bufferedSorce.close)
+      )
+    val repliesJsonFilepath = Paths.get(sBotConfig.repliesJsonFilename)
     for {
       _ <- IO.println(
         s"[DataEntry:22:42]] Read the input ${input.length} links & parse them to Json"
@@ -26,10 +36,10 @@ object DataEntry {
       mediafileSources <- parseInput(input)
       mediafileSourcesJson = mediafileSources.asJson
       _               <- IO.println("[DataEntry:24:45]] Read the json_list and parse it to json")
-      botListFile     <- jsonFileResource.use(_.mkString.pure[IO])
+      botListFile     <- jsonListFileResource.use(_.mkString.pure[IO])
       botListFileJson <- IO.fromEither(parse(botListFile))
-      _               <- IO.println("[DataEntry:26:45]] Merge the 2 arrays together")
-      mergedArray = {
+      _               <- IO.println("[DataEntry]] Merge the 2 arrays together list")
+      mergedArrayList = {
         // Extract the arrays as lists of Json elements
         val elements1 = botListFileJson.asArray.getOrElse(Vector.empty)
         val elements2 = mediafileSourcesJson.asArray.getOrElse(Vector.empty)
@@ -37,12 +47,25 @@ object DataEntry {
         Json.fromValues(elements1 ++ elements2)
       }
       _ <- IO.println("[DataEntry] Write the json back")
-      _ <- IO(Files.write(jsonFilePath, mergedArray.toString.getBytes(StandardCharsets.UTF_8)))
+      _ <- IO(Files.write(listJsonFilepath, mergedArrayList.toString.getBytes(StandardCharsets.UTF_8)))
 
       _ <- IO.println("[DataEntry] create media file source groups")
       mediaFileSourceGroups = MediaFileSourceGroup.fromMediaFileSourceList(mediafileSources)
       _ <- IO.println("[DataEntry] convert media file source groups to ReplyBundleMessages")
-    } yield s"""List(
-               |  ${mediaFileSourceGroups.map(MediaFileSourceGroup.toReplyBundleMessage).asJson.spaces2}
-               |)""".stripMargin
+      newReplyBundleMessages = mediaFileSourceGroups.map(MediaFileSourceGroup.toReplyBundleMessage).asJson
+      _ <- IO.println("[DataEntry] Read the current replies")
+      botRepliesFile     <- repliesJsonFileResource.use(_.mkString.pure[IO])
+      botRepliesFileJson <- IO.fromEither(parse(botRepliesFile))
+      _               <- IO.println("[DataEntry]] Merge the 2 arrays together replies")
+      mergedArrayReplies = {
+        // Extract the arrays as lists of Json elements
+        val elements1 = newReplyBundleMessages.asArray.getOrElse(Vector.empty)
+        val elements2 = botRepliesFileJson.asArray.getOrElse(Vector.empty)
+        // Combine and convert back to Json
+        Json.fromValues(elements1 ++ elements2)
+      }
+      _ <- IO.println("[DataEntry] Write the json back")
+      _ <- IO(Files.write(repliesJsonFilepath, mergedArrayReplies.toString.getBytes(StandardCharsets.UTF_8)))
+    } yield ()
+  }
 } // end DataEntry
